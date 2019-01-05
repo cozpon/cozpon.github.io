3011e2eb819302e0e5ad362363a8571fc36b037b
Ricky Peters <ericrickyallenpeters@gmail.com>
2019-01-05 14:44:03 -0800

    New Build

--- src/containers/Bar/index.js
+++ src/containers/Bar/index.js
@@ -3,11 +3,19 @@ import BarHeader from '../../components/barheader.components';
 class Bar extends Component {
   render() {
     return(
-      <div className="bar">
-        <BarHeader />
-        <div id="bar-text">
-          <h2> comin soon </h2>
-        </div>
+      <div className="main">
+      <header id="global-nav-header">
+          <BarHeader />
+      </header>
+          <br />
+          <center>
+            <img src="https://i.imgur.com/pfp1faA.png" alt="dridry" />
+          </center>
+          <br />
+          <div id="hours-text">
+            Hours: 7am - 5pm<br />
+            Monday thru Sunday
+          </div>
       </div>
     );
   }
